4d52f949940af3c5e12da8eed9a287c03c6238e7
uditgaurav <udit@chaosnative.com>
2023-08-10 19:06:50 +0530

    add windows binary
    Signed-off-by: uditgaurav <udit@chaosnative.com>

--- build/go-multiarch-build.sh
+++ build/go-multiarch-build.sh
@@ -8,15 +8,20 @@ fi
 package_split=(${package//\// })
 package_name=${package_split[-1]}
 
-# Add the architecture for building image 
-platforms=("linux/amd64" "linux/arm64" "darwin/arm64" "darwin/amd64")
+# Add the architecture for building image
+platforms=("linux/amd64" "linux/arm64" "darwin/arm64" "darwin/amd64" "windows/amd64" "windows/arm64")
 for platform in "${platforms[@]}"
 do
     platform_split=(${platform//\// })
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
 
-    env GOOS=$GOOS GOARCH=$GOARCH CGO_ENABLED=0 $package-$GOOS-$GOARCH
+    output_name="$package_name-$GOOS-$GOARCH"
+    if [[ $GOOS == "windows" ]]; then
+      output_name+=".exe" # Add the .exe extension for Windows binaries
+    fi
+
+    env GOOS=$GOOS GOARCH=$GOARCH CGO_ENABLED=0 go build -o $output_name $package
     if [ $? -ne 0 ]; then
         echo 'An error has occurred! Aborting the script execution...'
         exit 1
